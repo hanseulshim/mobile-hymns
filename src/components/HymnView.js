ce902fb7b2dccb15e08da91afb6cf2370247f077
hanseulshim <hanseulshim@gmail.com>
2018-04-05 11:43:42 -0400

    hymnview shows lyrics

--- src/components/HymnView.js
+++ src/components/HymnView.js
@@ -2,16 +2,37 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Text, View } from 'react-native';
 
-export default class HymnView extends Component {
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import * as Actions from '../actions';
+
+class HymnView extends Component {
+  static navigationOptions = ({ navigation }) => {
+    const { params } = navigation.state;
+
+    return {
+      title: params ? params.title : '',
+    };
+  };
+
   render() {
-    const { navigate } = this.props.navigation;
     return (
       <View>
-        <Text>POOPOOO</Text>
+        <Text>{this.props.lyrics}</Text>
       </View>);
   }
 }
 
+const mapStateToProps = state => ({
+  title: state.hymnReducer.title,
+  lyrics: state.hymnReducer.lyrics,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(Actions, dispatch);
+
 HymnView.propTypes = {
-  navigation: PropTypes.shape({ navigate: PropTypes.func }).isRequired,
+  navigation: PropTypes.shape({ setParams: PropTypes.func }).isRequired,
 };
+
+export default connect(mapStateToProps, mapDispatchToProps)(HymnView);

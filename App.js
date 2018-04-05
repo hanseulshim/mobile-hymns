e4974487984c6f7b84c0ca9490d9137301134d08
hanseulshim <hanseulshim@gmail.com>
2018-04-04 23:44:37 -0400

    update app to default to home

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
+import Home from './src/components/Home';
+import HymnView from './src/components/HymnView';
+import store from './src/store';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const SimpleApp = StackNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      title: 'Select a Hymn',
+    },
+  },
+  HymnView: {
+    screen: HymnView,
+    navigationOptions: {
+      title: 'jFeed',
+    },
   },
 });
+
+export default () => (
+  <Provider store={store}>
+    <SimpleApp />
+  </Provider>
+);
